c4333c0da0ca56685e73be33cfaaebe2002c0c5e
Dmitriy Zagrebin <dzagrebin@elvees.com>
2018-03-13 10:23:24 +0300

    python-serial: Update site URL
    Since v3.1 pySerial package URL has changed.
    
    Change-Id: I5998c418c31b4f500c96051dc6f27245bc055812
    Issue: #MCOM02SW-666

--- package/python-serial/python-serial.mk
+++ package/python-serial/python-serial.mk
@@ -6,7 +6,7 @@
 
 PYTHON_SERIAL_VERSION = 3.4
 PYTHON_SERIAL_SOURCE = pyserial-$(PYTHON_SERIAL_VERSION).tar.gz
-PYTHON_SERIAL_SITE = http://pypi.python.org/packages/source/p/pyserial
+PYTHON_SERIAL_SITE = https://pypi.python.org/packages/cc/74/11b04703ec416717b247d789103277269d567db575d2fd88f25d9767fe3d
 PYTHON_SERIAL_LICENSE = Python Software Foundation License
 PYTHON_SERIAL_LICENSE_FILES = LICENSE.txt
 PYTHON_SERIAL_SETUP_TYPE = distutils
