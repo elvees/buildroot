170d38484cb7c8b3c78b874e6d92bd16ebddadda
Vladimir Terekhov <vterekhov@elvees.com>
2022-02-10 20:10:19 +0300

    package/gst-omx: Fix build for mcom03 OMX target
    After change of package names mali-vpu-libs and mcom03-mali-vpu-libs
    to mcom03-vpu-libs and mcom03-vpu-libs-bin respectively gst-omx
    makefile should be fixed as it uses macros defined by these packages.
    If any of packages is enabled then gst-omx should be built for
    mcom03 OMX target.
    
    Issue: #MCOM03SW-1091
    Depends-On: https://gerrit.elvees.com/c/buildroot-external/mcom03/+/29602
    Change-Id: I6b0b788c2a7045ede70206711ade00a032d99f12

--- package/gstreamer1/gst-omx/gst-omx.mk
+++ package/gstreamer1/gst-omx/gst-omx.mk
@@ -21,7 +21,9 @@ GST_OMX_CONF_OPTS = \
 ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 GST_OMX_VARIANT = rpi
 GST_OMX_CONF_OPTS += -Dheader_path=$(STAGING_DIR)/usr/include/IL
-else ifeq ($(BR2_PACKAGE_MALI_VPU_LIBS),y)
+else ifeq ($(BR2_PACKAGE_MCOM03_VPU_LIBS),y)
+GST_OMX_VARIANT = mcom03
+else ifeq ($(BR2_PACKAGE_MCOM03_VPU_LIBS_BIN),y)
 GST_OMX_VARIANT = mcom03
 else
 GST_OMX_VARIANT = generic
