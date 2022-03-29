738ec8d2397cc22459f7d7c03086dc422155c0b0
Vladimir Terekhov <vterekhov@elvees.com>
2022-03-29 14:03:34 +0300

    package/gst-omx: Remove mcom03-vpu-libs-bin package handling
    mcom03-vpu-libs-bin is removed so we don't need to handle it in
    gst-omx package anymore.
    
    Issue: #MCOM03SW-1297
    Change-Id: If381bc951673f51c8aaea8794025248d2e687913

--- package/gstreamer1/gst-omx/gst-omx.mk
+++ package/gstreamer1/gst-omx/gst-omx.mk
@@ -23,8 +23,6 @@ GST_OMX_VARIANT = rpi
 GST_OMX_CONF_OPTS += -Dheader_path=$(STAGING_DIR)/usr/include/IL
 else ifeq ($(BR2_PACKAGE_MCOM03_VPU_LIBS),y)
 GST_OMX_VARIANT = mcom03
-else ifeq ($(BR2_PACKAGE_MCOM03_VPU_LIBS_BIN),y)
-GST_OMX_VARIANT = mcom03
 else
 GST_OMX_VARIANT = generic
 endif
