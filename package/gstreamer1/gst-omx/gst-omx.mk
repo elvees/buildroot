0361c6bd54918b8d9253cb9af5aa1be94476fa00
Omar Al-Wadi <oalwadi@elvees.com>
2023-10-23 11:28:35 +0300

    package/gstreamer1/gst-omx: Use plugins from ELVEES repos
    We use GStreamer monorepository [1] for the most GStreamer plugins but
    it hasn't have MCom-03 support in gst-omx yet so we get gst-omx from
    branch mcom03-1.18 in [2]. Currently it doesn't include changes from
    gst-omx in GStreamer 1.22.2. We tested that OMX encoders work
    successfully after updating GStreamer from 1.18.y to 1.22.2, but OMX
    decoders weren't tested.
    
    Since commit 5da82181846b ("support/download: drop support for the
    'none' hash") we can't set no hashes for files. If .hash exists,
    Buildroot will try to search for the proper hash, and it won't find it,
    so let's delete those files.
    
    [1] ssh://gerrit.elvees.com:29418/lib/gst-omx
    [2] ssh://gerrit.elvees.com:29418/lib/gstreamer
    
    Issue: #MCOM03SW-2520
    Change-Id: Ib6187f1739bb4b2811f5d0a4cf495d6bd2cdbed9
    (cherry picked from commit f22029cf3cf2eb6e8a02ffbf6d13597577f5f23c)

--- package/gstreamer1/gst-omx/gst-omx.mk
+++ package/gstreamer1/gst-omx/gst-omx.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-GST_OMX_VERSION = 1.22.2
-GST_OMX_SOURCE = gst-omx-$(GST_OMX_VERSION).tar.xz
-GST_OMX_SITE = https://gstreamer.freedesktop.org/src/gst-omx
+GST_OMX_VERSION = mcom03-1.18.y
+# TODO: Use monorepository ssh://gerrit.elvees.com:29418/lib/gstreamer after MCOM03SW-2542
+GST_OMX_SITE = ssh://gerrit.elvees.com:29418/lib/gst-omx
+GST_OMX_SITE_METHOD = git
+GST_OMX_GIT_SUBMODULES = YES
 
 GST_OMX_LICENSE = LGPL-2.1
 GST_OMX_LICENSE_FILES = COPYING
