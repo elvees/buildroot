8bf8aca5671ebecc7c91202a3e6f146029600e89
Vladimir Terekhov <vterekhov@elvees.com>
2024-01-25 12:16:39 +0300

    package/mesa3d: Change branch and license
    The new mcom03-17.0.y mesa3d branch doesn't contain any proprietary
    code, so the licence can be changed to MIT. Also this commit removes
    tarball creation.
    The same goes for the mesa3d-headers package.
    
    Issue: #MCOM03SW-2573
    Change-Id: I1d2c8256f96ce617cd600d3b57a6ee049f9be15f

--- package/mesa3d-headers/mesa3d-headers.mk
+++ package/mesa3d-headers/mesa3d-headers.mk
@@ -12,10 +12,10 @@ endif
 
 # Not possible to directly refer to mesa3d variables, because of
 # first/second expansion trickery...
-MESA3D_HEADERS_VERSION = powervr-17.0.y
+MESA3D_HEADERS_VERSION = mcom03-17.0.y
 MESA3D_HEADERS_SITE = ssh://gerrit.elvees.com:29418/tools/mesa
 MESA3D_HEADERS_SITE_METHOD = git
-MESA3D_HEADERS_LICENSE = Proprietary
+MESA3D_HEADERS_LICENSE = MIT
 MESA3D_HEADERS_LICENSE_FILES = docs/license.html
 
 # Only installs header files
@@ -34,9 +34,9 @@ ifeq ($(BR2_PACKAGE_XORG7),y)
 # contains -i.
 define MESA3D_HEADERS_BUILD_DRI_PC
 	sed -e 's:@\(exec_\)\?prefix@:/usr:' \
-	    -e 's:@libdir@:${exec_prefix}/lib:' \
-	    -e 's:@includedir@:${prefix}/include:' \
-	    -e 's:@DRI_DRIVER_INSTALL_DIR@:${libdir}/dri:' \
+	    -e 's:@libdir@:$(exec_prefix)/lib:' \
+	    -e 's:@includedir@:$(prefix)/include:' \
+	    -e 's:@DRI_DRIVER_INSTALL_DIR@:$(libdir)/dri:' \
 	    -e 's:@VERSION@:$(MESA3D_HEADERS_VERSION):' \
 	    -e 's:@DRI_PC_REQ_PRIV@::' \
 	    $(@D)/src/mesa/drivers/dri/dri.pc.in \
--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -5,18 +5,13 @@
 ################################################################################
 
 # When updating the version, please also update mesa3d-headers
-MESA3D_VERSION = powervr-17.0.y
+MESA3D_VERSION = mcom03-17.0.y
 MESA3D_SITE = ssh://gerrit.elvees.com:29418/tools/mesa
 MESA3D_SITE_METHOD = git
-MESA3D_LICENSE = Proprietary
+MESA3D_LICENSE = MIT
 MESA3D_LICENSE_FILES = docs/license.html
 MESA3D_AUTORECONF = YES
-
 MESA3D_INSTALL_STAGING = YES
-MESA3D_INSTALL_IMAGES = YES
-
-# Path to the directory with binaries for tarball creation
-MESA3D_TARGET_FILES_DIR=$(@D)/binaries
 
 MESA3D_PROVIDES =
 
@@ -213,16 +208,5 @@ endif
 
 # Avoid automatic search of llvm-config
 MESA3D_CONF_OPTS += --with-llvm-prefix=$(STAGING_DIR)/usr/bin
-
-define MESA3D_INSTALL_IMAGES_CMDS
-	# Create directory for binaries
-	rm -rf $(MESA3D_TARGET_FILES_DIR)
-	mkdir -p $(MESA3D_TARGET_FILES_DIR)
-
-	DESTDIR=$(MESA3D_TARGET_FILES_DIR) $(MAKE) -C $(@D) install
-
-	tar -C $(MESA3D_TARGET_FILES_DIR) -czf $(BINARIES_DIR)/mesa3d-$(MESA3D_VERSION).tar.gz .
-endef
-
 
 $(eval $(autotools-package))
