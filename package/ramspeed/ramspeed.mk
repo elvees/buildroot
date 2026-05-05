728f396a4a3e5c4102011b76f5fa0959ec7b770b
Maksim Tarelov <mtarelov@elvees.com>
2026-05-05 12:54:51 +0300

    package/ramspeed: Change source link
    The source [1] specified in the recipe is unavailable since 2023. So,
    we changed it to [2].
    
    [1] http://www.alasir.com/software/ramspeed
    [2] https://github.com/cruvolo/ramspeed/tree/upstream/ramspeed-2.6.0
    
    Issue: #MCOM03SW-3864
    Change-Id: Ie53fa78fbfcd2080262f1dbf0af88e1c6f41e21e

--- package/ramspeed/ramspeed.mk
+++ package/ramspeed/ramspeed.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-RAMSPEED_VERSION = 2.6.0
-RAMSPEED_SITE = http://www.alasir.com/software/ramspeed
+RAMSPEED_VERSION = upstream/ramspeed-2.6.0
+RAMSPEED_SITE = https://github.com/cruvolo/ramspeed
+RAMSPEED_SITE_METHOD = git
 RAMSPEED_ARCH = $(if $(BR2_i386),i386)$(if $(BR2_x86_64),x86_64)
 RAMSPEED_LICENSE = Alasir License
 RAMSPEED_LICENSE_FILES = LICENCE
